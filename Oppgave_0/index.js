91577526297ea6eba18dd280c0525d891ee7c5ee
snilseen <nilzen@live.com>
2024-08-27 14:00:00 +0200

    Gjort ferdig alle oppgavene.

--- Oppgave_0/index.js
+++ Oppgave_0/index.js
@@ -43,3 +43,54 @@ writeListButton.addEventListener("click", () => {
 });
 
 // Oppgave 5
+
+const placeholder = document.getElementById("placeholder");
+const select = document.getElementById("select");
+const text = document.getElementById("text");
+const createButton = document.getElementById("create");
+
+createButton.addEventListener("click", () => {
+  const htmlElement = document.createElement(select.value);
+  htmlElement.textContent = text.value;
+  placeholder.appendChild(htmlElement);
+
+  text.value = "";
+});
+
+// Oppgave 6
+
+const listItem = document.getElementById("list");
+const removeListItemButton = document.getElementById("remove-li");
+
+removeListItemButton.addEventListener("click", () => {
+  listItem.removeChild(listItem.lastElementChild);
+});
+
+// Oppgave 7
+
+const inputNameField = document.getElementById("name");
+const orderButton = document.getElementById("order");
+
+inputNameField.addEventListener("input", () => {
+  if (inputNameField.value.length > 4) {
+    orderButton.disabled = true;
+    orderButton.style.borderColor = "red";
+  } else {
+    orderButton.disabled = true;
+    orderButton.style.borderColor = "";
+  }
+});
+
+// Oppgave 8
+
+const colorButton = document.getElementById("color");
+
+colorButton.addEventListener("click", () => {
+  document.querySelectorAll(".children li:nth-child(odd)").forEach((item) => {
+    item.style.border = "2px solid green";
+  });
+
+  document.querySelectorAll(".children li:nth-child(even)").forEach((item) => {
+    item.style.border = "2px solid pink";
+  });
+});
